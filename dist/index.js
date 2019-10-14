43e1fce4ea0b01505cdb1b4f27fbf5fdfee78a2f
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-10-14 16:34:25 +0200

    Fix: build toooo

--- dist/index.js
+++ dist/index.js
@@ -8447,11 +8447,16 @@ const init = () => {
 
 const getFileContent = async (path) => {
   try {
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
     const { data: res } = await octokit.repos.getContents({
+      owner,
+      repo,
       path
     })
     const buff = new Buffer(res, 'base64');
     const fileContent = buff.toString('ascii');
+    console.log(fileContent);
     return fileContent;
   } catch (e) {
     throw e;
