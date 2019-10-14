37f77c87095ee3c1bb3c0adb28e1fe50c865f92e
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-10-14 17:13:31 +0200

    Fix: debuggin

--- dist/index.js
+++ dist/index.js
@@ -505,14 +505,20 @@ const checkNodeVersion = (path) => {
       const filePath = `${path}/package.json`;
       const file = getFileContent(filePath);
       const oldPkgJson = JSON.parse(file)
+      console.log("JSON VERSION")
+      console.log(oldPkgJson)
       console.log(oldPkgJson.version)
       const newPgkJson = __webpack_require__(169)(filePath);
+      console.log("NEW PACKAGS JOSN")
+      console.log(newPgkJson);
       if(oldPkgJson.version === newPgkJson.version) {
         core.setFailed('Need a new package.json version');
         return;
       }
       console.log(`Package.json check all good`);
     } catch (e) {
+      console.log('ERRROR');
+      console.log(e);
       throw e;
     }
   }
