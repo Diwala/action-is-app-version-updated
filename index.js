59658876bb4f119b0b96cc6076195c1e6ed9c444
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-10-14 15:48:39 +0200

    Feature: intial commit
    First initial version up and running, and will test it now

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const { init, getFileContent } = require('./lib/github');
+const { checkNodeVersion } = require('./lib/node');
+try {
+  init();
+
+  const typeCheck = core.getInput('type-check');
+  const path = core.getInput('file-path');
+
+  if(typeCheck === 'node') {
+    checkNodeVersion(path);
+  }
+  if(typeCheck === 'android') {
+    const filePath = `${path}/build.gradle`;
+    const file = getFileContent(filePath);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
