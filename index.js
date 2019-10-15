ba06497b0850463fa2016db846f5a57dad724727
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-10-15 12:19:43 +0200

    Fix: Some cleanup, fixing
    Introducde eslint, husky and prettier

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-console */
 const core = require('@actions/core');
 const { init, getFileContent } = require('./lib/github');
 const { checkNodeVersion } = require('./lib/node');
+
 try {
   init();
 
   const typeCheck = core.getInput('type-check');
   const path = core.getInput('file-path');
 
-  if(typeCheck === 'node') {
+  if (typeCheck === 'node') {
     checkNodeVersion(path);
   }
-  if(typeCheck === 'android') {
+  if (typeCheck === 'android') {
     const filePath = `${path}/build.gradle`;
     const file = getFileContent(filePath);
+    console.log(file);
   }
 } catch (error) {
   core.setFailed(error.message);
